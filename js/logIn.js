53701448533fd5ad43ee44c2e0a354fcbf7aa20e
victor <victorbejar2005@gmail.com>
2024-05-31 16:18:36 +0200

    Pagina terminada
    Se han hecho como se vería para acceder a las guías

--- js/logIn.js
+++ js/logIn.js
@@ -1,28 +1,32 @@
 function emailCorrecto() {
-    console.log("Hola Mundo");
-    // Si hay mensajes de error se quitan
-    document.getElementById('emailError').textContent = '';
-    document.getElementById('passwordError').textContent = '';
+  // Si hay mensajes de error se quitan
+  document.getElementById('emailError').textContent = '';
+  document.getElementById('passwordError').textContent = '';
 
-    // Get form values
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  // Get form values
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    // Según he entendido esto hace que solo se valido el texto (email) cuando haya texto+@+texto+.+texto 
-    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Según he entendido esto hace que solo se valido el texto (email) cuando haya texto+@+texto+.+texto 
+  const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    let valid = true;
+  let valid = true;
 
-    // miramos que segun el patron dado anteriormente sea correcto la contraseña
-    if (!emailValido.test(email)) {
-      document.getElementById('emailError').textContent = 'La direccón del email debe ser correcta.';
-      valid = false;
-    }
+  // miramos que segun el patron dado anteriormente sea correcto la contraseña
+  if (!emailValido.test(email)) {
+    document.getElementById('emailError').textContent = 'La direccón del email debe ser correcta.';
+    valid = false;
+  }
 
-    // miramos si la contraseña tiene mas de 8 caracteres
-    if (password.length < 8) {
-      document.getElementById('passwordError').textContent = 'Una contraseña debe tener como minimo 8 caracteres.';
-      valid = false;
-    }
+  // miramos si la contraseña tiene mas de 8 caracteres
+  if (password.length < 8) {
+    document.getElementById('passwordError').textContent = 'Una contraseña debe tener como minimo 8 caracteres.';
+    valid = false;
+  }
 
+  if(valid){
+    //Esto ahora no funciona pero antes te llevaba a la pagina principal 
+    var url =  window.location.hostname+":"+window.location.port+"/html/howplay.html";
+    window.location.replace(url);
+  }
 }
